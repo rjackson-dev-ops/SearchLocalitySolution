ff8bb6ed2e4e1b94ea6e525461ae29e4e02be586
Robert Jackson <rjack2@gmail.com>
2017-08-27 04:33:53 -0500

    Search working
    Search working

--- search_locality.rb
+++ search_locality.rb
@@ -1,8 +1,54 @@
 class LocalitySearch
 
-  def get_files(invalid_directory)
-    
-    raise 'Directory invalid'
+  def initialize(first_term, second_term, number_words_between, match_string)
+
+    @first_term = first_term
+    @second_term = second_term
+    @number_words_between = number_words_between
+    @match_string = match_string
+  end
+
+  def get_files(directory)
+    raise('Directory invalid') unless (directory_exists?(directory))
+
+    path = directory + '/*.txt'
+
+    file_list = Dir[path]
+  end
+
+  def directory_exists?(directory)
+    File.directory?(directory)
+  end
+
+  def find_matching_strings()
+    words = @match_string.split(/\W+/)
+
+    match_list = []
+
+    first_list = []
+    second_list = []
+
+    # save teh indices when we find that either term matches
+    (0..words.size-1).each do |index|
+      next_word = words[index]
+      first_list.push(index) if @first_term.eql? next_word
+      second_list.push(index) if @second_term.eql? next_word
+    end
+
+    # compare the lists to see if any words are within the search range
+    (0..first_list.size-1).each  do |first_list_index|
+      (0..second_list.size-1).each  do |second_list_index|
+
+        first_term_index = first_list[first_list_index]
+        second_term_index = second_list[second_list_index]
+        current_range = (first_term_index - second_term_index).abs
+
+        match_list.push([first_term_index, second_term_index]) if(current_range <= @number_words_between)
+
+      end
+  end
+
+    match_list
   end
 
 end
--- test/search_locality_spec.rb
+++ test/search_locality_spec.rb
@@ -2,13 +2,59 @@ require 'rspec'
 require_relative '../search_locality'
 
 describe 'Search Locality' do
-  let(:invalid_directory) {"bogus_directory"}
+  let(:directory_to_search) {""}
   let(:first_term) {"developer"}
   let(:second_term) {"operations"}
-  let(:localitySearch) {LocalitySearch.new}
+  let(:number_words_between) {9}
+  let(:string_match) {""}
+  let(:localitySearch) {LocalitySearch.new(first_term, second_term, number_words_between, string_match)}
+  let(:non_match_string) {"A tale of non-matched strings for testing."}
+  let(:match_string) {"The " + first_term + " should also be involved in " + second_term + "."}
 
   it 'should return an error message if the directory is not valid' do
-    expect{localitySearch.get_files(invalid_directory)}.to raise_error('Directory invalid')
+    directory_to_search = 'bogus_directory'
+    expect{localitySearch.get_files(directory_to_search)}.to raise_error('Directory invalid')
   end
+
+  it 'should return an empty list if the directory is empty' do
+    directory_to_search = '../data/empty_directory'
+    file_list = localitySearch.get_files(directory_to_search)
+    expect(file_list).to be_empty
+  end
+
+  it 'should return one file when the directory has one text file' do
+    directory_to_search = '../data/directory_with_file'
+    file_list = localitySearch.get_files(directory_to_search)
+    file_list_size = file_list.size
+    expect(file_list_size).to eq(1)
+  end
+
+  it 'should return one file when the directory has one text file and other non text files' do
+    directory_to_search = '../data/directory_with_non_text_files'
+    file_list = localitySearch.get_files(directory_to_search)
+    file_list_size = file_list.size
+    expect(file_list_size).to eq(1)
+  end
+
+  context 'Non matching string' do
+    let(:string_match) {non_match_string}
+
+    it 'should return an empty list if string to be matched does not contain a match' do
+      match_list  = localitySearch.find_matching_strings()
+      expect(match_list).to be_empty
+    end
+  end
+
+  context 'Matching string' do
+    let(:string_match) {match_string}
+    it 'should return a list with one entry if the match string contains one match' do
+      match_list  = localitySearch.find_matching_strings()
+      match_list_size = match_list.size
+      expect(match_list_size).to eq(1)
+    end
+  end
+
+
+
 
 end
